d2efff725c3b7fa9e8940aad1158ec878530dce0
Nastaran vaezipoor <nastaranvaezipoor@gmail.com>
2025-12-25 23:46:30 +0330

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+#تمرین_شماره_یک
+name = input ("please enter your name: ") 
+brith_year_ask = input("please enter your brith year: (like this:1367) ")
+brith_year = int(brith_year_ask)
+this_year = 1404
+age = this_year - brith_year
+print(f"hi {name} you are {age} years old.")
